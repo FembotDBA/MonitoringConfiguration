fb03a99a642d4c43e4083cdd18e68a5dfe6634e6
fembotdba <lararasner@gmail.com>
2015-02-19 23:07:21 -0800

    Syning

---
+++ Config.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace MonitoringConfiguration
+{
+    partial class Config
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lvServers = new System.Windows.Forms.ListView();
+            this.btnTask = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(148, 14);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(435, 20);
+            this.txtConnectionString.TabIndex = 0;
+            this.txtConnectionString.TextChanged += new System.EventHandler(this.txtConnectionString_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Connection String";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(627, 13);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lvServers
+            // 
+            this.lvServers.FullRowSelect = true;
+            this.lvServers.Location = new System.Drawing.Point(31, 53);
+            this.lvServers.MultiSelect = false;
+            this.lvServers.Name = "lvServers";
+            this.lvServers.Size = new System.Drawing.Size(776, 200);
+            this.lvServers.TabIndex = 4;
+            this.lvServers.UseCompatibleStateImageBehavior = false;
+            this.lvServers.View = System.Windows.Forms.View.Details;
+            // 
+            // btnTask
+            // 
+            this.btnTask.Location = new System.Drawing.Point(42, 289);
+            this.btnTask.Name = "btnTask";
+            this.btnTask.Size = new System.Drawing.Size(75, 23);
+            this.btnTask.TabIndex = 5;
+            this.btnTask.Text = "Run Task";
+            this.btnTask.UseVisualStyleBackColor = true;
+            this.btnTask.Click += new System.EventHandler(this.btnTask_Click);
+            // 
+            // Config
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 615);
+            this.Controls.Add(this.btnTask);
+            this.Controls.Add(this.lvServers);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtConnectionString);
+            this.Name = "Config";
+            this.Text = "Configuration";
+            this.Load += new System.EventHandler(this.Config_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ListView lvServers;
+        private System.Windows.Forms.Button btnTask;
+    }
+}
+
---
+++ Config.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MonitoringConfiguration.Properties;
+using MonitoringModel;
+
+namespace MonitoringConfiguration
+{
+    public partial class Config : Form
+    {
+        public Config()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            List<MonitoredServer> monitoredServers = GetMonitoredServers(txtConnectionString.Text);
+            FillListView(monitoredServers);
+        }
+
+        private void FillListView(List<MonitoredServer> monitoredServers)
+        {
+
+            lvServers.Columns.Add("Server Name");
+            lvServers.Columns.Add("Server Id");
+            lvServers.Columns.Add("Connection String");
+            foreach(MonitoredServer monitoredServer in monitoredServers)
+            {
+                //ListViewItem listItem = new ListViewItem(monitoredServer.Id.ToString());
+                ListViewItem listItem = new ListViewItem(monitoredServer.ServerName) { Tag = monitoredServer };
+                listItem.SubItems.Add(monitoredServer.Id.ToString());
+                listItem.SubItems.Add(monitoredServer.ConnString.ToString());
+                lvServers.Items.Add(listItem);
+            }
+            lvServers.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+        
+        static List<MonitoredServer> GetMonitoredServers(string connectionString)
+        {
+            List<MonitoredServer> MonitoredServers = new List<MonitoredServer>();
+            
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                connection.Open();
+                using (SqlCommand command = new SqlCommand("SELECT * FROM dbo.MonitoredServer", connection))
+                {
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    try
+                    {
+                        while (reader.Read())
+                        {
+                            MonitoredServers.Add(new MonitoredServer
+                            {
+                                ServerName = reader["ServerName"].ToString()
+                                ,
+                                Id = Int32.Parse(reader["ServerId"].ToString())
+                                ,
+                                ConnString = reader["ConnString"].ToString()
+                            });
+                        }
+                    }
+                    finally
+                    {
+                        reader.Close();
+                    }
+                }
+            }
+
+            return MonitoredServers;
+        }
+
+        private void btnTask_Click(object sender, EventArgs e)
+        {
+            getItemInfo();
+        }
+
+        private void getItemInfo()
+        {
+            //make sure row is selected
+            if (lvServers.SelectedItems.Count == 1)
+            {
+                runTask(Int32.Parse(lvServers.SelectedItems[0].SubItems[1].Text), lvServers.SelectedItems[0].SubItems[2].Text);
+            }
+        }
+
+        private void runTask(int id, string connString)
+        {
+
+            string monitoredConnString = Settings.Default.MonitoredDBConnString;
+            string sql = "";
+            string table = "";
+
+            DataTable dtResults = new DataTable();
+
+            //read sql to execute from db
+            using (SqlConnection connMonitorDB = new SqlConnection(monitoredConnString))
+            {
+                using (SqlCommand command = new SqlCommand("SELECT MonitoredSQL, DataTable FROM dbo.MonitoredTask WHERE MonitoredTaskName = 'File Latency'", connMonitorDB))
+                {
+                    connMonitorDB.Open();
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    try
+                    {
+                        while (reader.Read())
+                        {
+                            sql = reader["MonitoredSQL"].ToString().Replace("%ServerId%",id.ToString());
+                            table = reader["DataTable"].ToString();
+                        }
+                    }
+                    finally
+                    {
+                        reader.Close();
+                    }
+                }
+            }
+           
+            //execute task sql on remote server and save results to Monitoring DB
+            using (SqlConnection connRemote = new SqlConnection(connString))
+            {
+
+                using (SqlCommand commTask = new SqlCommand(sql, connRemote))
+                {
+                    connRemote.Open();
+
+                    SqlDataAdapter da = new SqlDataAdapter(commTask);
+
+                    da.Fill(dtResults);
+                    connRemote.Close();
+                    da.Dispose();
+
+                }
+            }
+             
+            using (SqlConnection connSaveResults = new SqlConnection(monitoredConnString))
+            {
+                using (SqlCommand commSaveResults = connSaveResults.CreateCommand())
+                {
+                    //save results to table
+                    commSaveResults.CommandText = "dbo.SaveFileLatency";
+                    commSaveResults.CommandType = CommandType.StoredProcedure;
+
+                    SqlParameter parameter;
+                    parameter = commSaveResults.Parameters.AddWithValue("@fileLatency", dtResults);
+                    parameter.SqlDbType = SqlDbType.Structured;
+                    parameter.TypeName = "data.FileLatency";
+
+                    commSaveResults.ExecuteNonQuery();
+                }
+            }
+            
+            
+        }
+
+        private void txtConnectionString_TextChanged(object sender, EventArgs e)
+        {
+            Settings.Default.MonitoredDBConnString = txtConnectionString.Text;
+            Settings.Default.Save();
+        }
+
+        private void Config_Load(object sender, EventArgs e)
+        {
+            txtConnectionString.Text = Settings.Default.MonitoredDBConnString;
+        }
+
+        
+    }
+}
